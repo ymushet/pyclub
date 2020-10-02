7e31fecc8727f914f5a2b647f1e49587ed473541
ymushet <18755772+ymushet@users.noreply.github.com>
2020-10-02 23:48:57 +0300

    problems/merge_intervals: add is_overlap tests

--- problems/merge_intervals.py
+++ problems/merge_intervals.py
@@ -2,6 +2,29 @@ from typing import List
 import doctest
 
 
+def is_overlap(a : int, b : int, c : int, d : int) -> bool:
+    """"
+    Determine if [a, b] and [c, d] overlaps
+    
+    >>> is_overlap(1, 2, 2, 3)
+    True
+    >>> is_overlap(1, 1, 1, 1)
+    True
+    >>> is_overlap(1, 2, 1, 2)
+    True
+    >>> is_overlap(1, 2, 3, 4)
+    False
+    """
+    if b == c:
+        return True
+    elif a >= c and c <= b:
+        return True
+    elif a >= d and d <= b:
+        return True
+
+    return False
+
+
 def merge(intervals: List[List[int]]) -> List[List[int]]:
     """
     See https://leetcode.com/problems/merge-intervals/
@@ -12,11 +35,8 @@ def merge(intervals: List[List[int]]) -> List[List[int]]:
     [[1, 5]]
     """
     intervals.sort(key=lambda a: a[0])
+    # TODO: Merge lists now
 
 
 if __name__ == "__main__":
-    """
-    merge([[5, 6], [3, 4], [1, 2]])
-    >>> [[1, 7], [3, 4], [5, 6]]
-    """
-    doctest.testmod()
+    doctest.run_docstring_examples(is_overlap, globals())
