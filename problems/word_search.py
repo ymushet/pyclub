403bb867c3aaf9b91eb41db653dbbc2f6b26feee
ymushet <18755772+ymushet@users.noreply.github.com>
2020-11-22 21:49:16 +0200

    [wip] recurion solution

--- problems/word_search.py
+++ problems/word_search.py
@@ -1,3 +1,5 @@
+import doctest
+
 """
 See https://leetcode.com/problems/word-search/
 
@@ -16,7 +18,6 @@ The same letter cell may not be used more than once.
 "ABCBQ" => False
 """
 
-
 def main(board, word):
     """
     >>> main([['A','B','C','E'],['S','F','C','S'],['A','D','E','E']], "ABCCED")
@@ -26,3 +27,38 @@ def main(board, word):
     >>> main([['A','B','C','E'],['S','F','C','S'],['A','D','E','E']], "ABCBQ")
     False
     """
+    for i, row in enumerate(board):
+        for j, square in enumerate(row):
+            if square == word[0]:
+                res = neighbors(board, word[1:], i, j)
+                if res:
+                    return True
+    return False
+
+def neighbors(board, word, i, j):
+    if len(word) == 1:
+        return True
+
+    w = word[0]
+    print(w)
+    board[i][j] = '#'
+
+    x = len(board) # height
+    y = len(board[0]) # weight
+
+    if i - 1 >= 0 and w == board[i - 1][j]:
+        neighbors(board, word[1:], i - 1, j)
+    # bottom
+    elif i + 1 <= x and w == board[i + 1][j]:
+        neighbors(board, word[1:], i + 1, j)
+    # left
+    elif j - 1 >= 0 and w == board[i][j - 1]:
+        neighbors(board, word[1:], i, j - 1)
+    # right
+    elif j + 1 <= y and w == board[i][j + 1]:
+        neighbors(board, word[1:], i, j + 1)
+
+    return False
+
+if __name__ == "__main__":
+    doctest.testmod()
