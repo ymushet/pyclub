b77f47f5aaa0d740f32c26559a5dc9f18910982c
ymushet <18755772+ymushet@users.noreply.github.com>
2020-12-02 18:37:27 +0200

    basic tests done

--- problems/word_search.py
+++ problems/word_search.py
@@ -26,39 +26,51 @@ def main(board, word):
     True
     >>> main([['A','B','C','E'],['S','F','C','S'],['A','D','E','E']], "ABCBQ")
     False
+    >>> main([["C","A","A"],["A","A","A"],["B","C","D"]], "AAB")
+    True
     """
     for i, row in enumerate(board):
         for j, square in enumerate(row):
             if square == word[0]:
-                res = neighbors(board, word[1:], i, j)
+                res = neighbors(board, word, i, j)
                 if res:
                     return True
     return False
 
 def neighbors(board, word, i, j):
-    if len(word) == 1:
+    if len(word) == 1 and board[i][j] == word[0]:
         return True
 
-    w = word[0]
-    print(w)
+    w = word[1]
     board[i][j] = '#'
+    x = len(board) - 1 # height
+    y = len(board[0]) - 1 # weight
 
-    x = len(board) # height
-    y = len(board[0]) # weight
-
+    # top
     if i - 1 >= 0 and w == board[i - 1][j]:
-        neighbors(board, word[1:], i - 1, j)
+        res = neighbors(board, word[1:], i - 1, j)
+        if res:
+            return True
     # bottom
-    elif i + 1 <= x and w == board[i + 1][j]:
-        neighbors(board, word[1:], i + 1, j)
+    if i + 1 <= x and w == board[i + 1][j]:
+        res = neighbors(board, word[1:], i + 1, j)
+        if res:
+            return True
     # left
-    elif j - 1 >= 0 and w == board[i][j - 1]:
-        neighbors(board, word[1:], i, j - 1)
+    if j - 1 >= 0 and w == board[i][j - 1]:
+        res = neighbors(board, word[1:], i, j - 1)
+        if res:
+            return True
     # right
-    elif j + 1 <= y and w == board[i][j + 1]:
-        neighbors(board, word[1:], i, j + 1)
+    if j + 1 <= y and w == board[i][j + 1]:
+        res = neighbors(board, word[1:], i, j + 1)
+        if res:
+            return True
 
     return False
 
 if __name__ == "__main__":
     doctest.testmod()
+    # main([["C","A","A"],
+    #       ["A","A","A"],
+    #       ["B","C","D"]],"AAB")
